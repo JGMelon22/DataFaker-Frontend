8582c22eb7951d00ad7535be0b4e7b17909a9e44
JGMelon22 <jgvasconcellos22@gmail.com>
2023-12-22 09:38:44 -0300

    feat: implementing get request with axios table of content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
+import springLogo from './assets/spring.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const baseUlr = 'http://localhost:8080/api/person'
+  const [data, setData] = useState([])
+
+  // Get People
+  const requestGet = async () => {
+    await axios.get(baseUlr)
+      .then(response => {
+        setData(response.data)
+      }).catch(error => {
+        alert(error)
+      })
+  }
+
+  useEffect(() => {
+    requestGet();
+  })
 
   return (
     <>
-      
+      <nav className='navbar navbar-expand-lg bg-primary' data-bs-theme='dark'>
+        <span className='text-white'>People</span>
+        <img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/devicon/devicon-plain.svg' />
+      </nav>
+
+      <table className='table table-bordered table-hover'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((person, index) => (
+            <tr key={index}>
+              <td>{person.id}</td>
+              <td>{person.firstName}</td>
+              <td>{person.lastName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
